990b056e3b9d2943e36bebcb6cccb29d8a2b1648
Joe <joe@xxtime.com>
2018-01-24 17:43:36 +0800

    length uint16

--- network/package.go
+++ network/package.go
@@ -6,17 +6,18 @@ import (
 	"bytes"
 )
 
-var length int32 = 0
-var headerSize int = 5
+var (
+	length     uint16 = 0
+	headerSize int    = 2
+)
 
 type Package struct {
 	BufferSize int
 }
 
 func (P *Package) Package(message string) ([]byte, error) {
-	var length int32 = int32(len(message)) + int32(headerSize)
+	var length uint16 = uint16(len(message)) + uint16(headerSize)
 	var pkg *bytes.Buffer = new(bytes.Buffer)
-	binary.Write(pkg, binary.BigEndian, []byte{0x0a})
 	binary.Write(pkg, binary.BigEndian, length)
 	err := binary.Write(pkg, binary.BigEndian, []byte(message))
 	if err != nil {
@@ -25,25 +26,24 @@ func (P *Package) Package(message string) ([]byte, error) {
 	return pkg.Bytes(), nil
 }
 
-func (P *Package) UnPackage(reader *bufio.Reader) ([]byte, int32, error) {
+func (P *Package) UnPackage(reader *bufio.Reader) ([]byte, uint16, error) {
 
 	// TODO :: if make []byte every time right
 	var buffer = make([]byte, P.BufferSize)
 
-	// header
-	header, err := reader.Peek(headerSize)
-	if err != nil {
-		return nil, 0, err
-	}
-
-	// reader.Buffered()
-	if length == 0 && header[0] == 0x0a {
-		err := binary.Read(bytes.NewBuffer(header[1:5]), binary.BigEndian, &length)
+	if length == 0 {
+		header, err := reader.Peek(headerSize)
 		if err != nil {
 			return nil, 0, err
 		}
+
+		err = binary.Read(bytes.NewBuffer(header[0:headerSize]), binary.BigEndian, &length)
+		if err != nil {
+			return nil, 0, err
+		}
+
 		reader.Discard(headerSize)
-		length -= int32(headerSize)
+		length -= uint16(headerSize)
 	}
 
 	// read
@@ -51,7 +51,7 @@ func (P *Package) UnPackage(reader *bufio.Reader) ([]byte, int32, error) {
 	if err != nil {
 		return nil, 0, err
 	}
-	length -= int32(len)
+	length -= uint16(len)
 	if length < 0 {
 		return nil, 0, err
 	}
